d0ad2ec82c1389a6a68367f5d6f31435331e50dd
João Victor <90943414+Milk21291@users.noreply.github.com>
2023-03-28 20:44:52 -0300

    Add files via upload

--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -1,14 +1,17 @@
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.awt.Color;
-import java.awt.Font;
 import java.io.InputStream;
+import java.awt.FontMetrics;
+import java.awt.font.TextLayout;
 
 import javax.imageio.ImageIO;
 public class StickerGenerator {
 
-    public void create(InputStream inputStream, String fileName) throws Exception {
+    public <Shap> void create(InputStream inputStream, String fileName) throws Exception {
         // InputStream inputStream = new FileInputStream(new File("entrada/movie.jpg"));
         // InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_9.jpg").openStream();
         BufferedImage imagemOriginal = ImageIO.read(inputStream);
@@ -25,11 +28,29 @@ public class StickerGenerator {
 
         // configurar a fonte
         var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 100);
-        graphics.setColor(Color.RED);
+        graphics.setColor(Color.YELLOW);
         graphics.setFont(fonte);
 
         // escrever uma frase na nova imagem
-        graphics.drawString("Filme:",380, novaAltura - 100);
+        String text = "Filme:";
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D rectangle = fontMetrics.getStringBounds(text, graphics);
+        int textWidth = (int) rectangle.getWidth();
+        int positionTextX = (largura - textWidth) / 2;
+        int positionTextY = novaAltura - 100;
+        graphics.drawString(text, positionTextX, positionTextY);
+
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        var textLayout = new TextLayout(text, fonte, fontRenderContext);
+        Shap outline = (Shap) textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(positionTextX, positionTextY);
+        graphics.setTransform(transform);
+        var outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+        graphics.setColor(Color.BLACK);
+        graphics.draw((Shape) outline);
+        graphics.setClip((Shape) outline);
 
 
         // escrever a nova imagem em um arquivo
